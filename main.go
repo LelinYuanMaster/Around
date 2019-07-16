21f6a363330e6ef90b3a0b7a2f52a8c5d47dccd3
LelinYuanMaster <45909112+LelinYuanMaster@users.noreply.github.com>
2019-07-16 01:10:22 -0700

    Add files via upload

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
  "reflect"
  "io"
 "github.com/pborman/uuid"
+ "cloud.google.com/go/bigtable"
  "github.com/auth0/go-jwt-middleware"
   "github.com/dgrijalva/jwt-go"
   "github.com/gorilla/mux"
@@ -28,8 +29,8 @@ const (
  TYPE = "post"
  DISTANCE = "200km"
  // Needs to update
- //PROJECT_ID = "around-xxx"
- //BT_INSTANCE = "around-post"
+ PROJECT_ID = "around-246007"
+ BT_INSTANCE = "around-post"
  // Needs to update this URL if you deploy it to cloud.
  ES_URL = "http://104.154.62.139:9200"
  BUCKET_NAME = "post-images-246007"
@@ -160,6 +161,7 @@ func handlerPost(w http.ResponseWriter, r*http.Request){
 	p.Url = attrs.MediaLink
 
 	saveToES(p,id)
+	saveToBigTable(p,id)
 }
 
 func saveToGCS(ctx context.Context, r io.Reader, buckerName, name string)(*storage.ObjectHandle,*storage.ObjectAttrs,error){
@@ -195,9 +197,32 @@ func saveToGCS(ctx context.Context, r io.Reader, buckerName, name string)(*stora
       attrs, err := obj.Attrs(ctx)
       fmt.Printf("Post is saved to GCS: %s\n", attrs.MediaLink)
       return obj, attrs, err
-	
 
 }
+
+func saveToBigTable(p *Post,id string){
+	ctx := context.Background()        // you must update project name here        
+	bt_client, err := bigtable.NewClient(ctx, PROJECT_ID, BT_INSTANCE)
+	if err != nil {
+		panic(err)
+		return
+	}
+
+         tbl:=bt_client.Open("post")
+	 mut := bigtable.NewMutation()
+	 t:=bigtable.Now()
+	 mut.Set("post","user",t,[]byte(p.User))
+	 mut.Set("post","message",t,[]byte(p.Message))
+	 mut.Set("location","lat",t,[]byte(strconv.FormatFloat(p.Location.Lat,'f',-1,64)))
+	 mut.Set("location","lon",t,[]byte(strconv.FormatFloat(p.Location.Lon,'f',-1,64)))
+         err = tbl.Apply(ctx, id, mut)
+		if err != nil {
+		panic(err)
+		return        }
+         fmt.Printf("Post is saved to BigTable: %s\n", p.Message)
+
+}
+
 
 
 func saveToES(p *Post, id string){
